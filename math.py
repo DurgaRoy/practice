b8c9fc6bea21fac994e2e4421c9dd76efcd89e74
Durga Shankar Roy <1999durgashankar@gmail.com>
2022-01-28 23:22:06 +0530

    adding a file first time

---
+++ math.py
@@ -0,0 +1,11 @@
+# add implementation
+def add(x,y):
+    pass
+# subtract implementation
+def subtract(x,y):
+    pass
+# multiply implementation
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass   
